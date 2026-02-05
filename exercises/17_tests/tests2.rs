208f128d9298e8ec2b08d1fbaea12cec5217f8af
Sarah Ma <sarahmma24@gmail.com>
2026-02-05 12:47:43 -0800

    errors, generics, traits, lifetimes, tests, iterators

--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -12,12 +12,12 @@ fn main() {
 mod tests {
     use super::*;
 
-    #[test]
+    #[cfg(test)]
     fn you_can_assert_eq() {
         // TODO: Test the function `power_of_2` with some values.
-        assert_eq!();
-        assert_eq!();
-        assert_eq!();
-        assert_eq!();
+        assert_eq!(power_of_2(3), 8);
+        assert_eq!(power_of_2(2), 4);
+        assert_eq!(power_of_2(1), 1);
+        assert_eq!(power_of_2(0), 1);
     }
 }
